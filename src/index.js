89316ab6e85cabf30e6ceceb626df4313c0e180f
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-05-20 15:08:04 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+
+const Hi = () => {
+  return <p>Hi.</p>;
+};
+render(<Hi />, document.getElementById("app"));
---
+++ webpack.config.dev.js
@@ -0,0 +1,5 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
