b4eb7334f862bfde12878cdab9faf0940b70d586
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-05-20 15:15:56 -0400

    implementing webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,3 +3,22 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 process.env.NODE_ENV = "development";
+module.exports = {
+  mode: "development",
+  target: "web",
+  devTool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+  },
+};
