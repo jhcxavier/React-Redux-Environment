271a1f8f68856a513b031fda86684887d53dafa1
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-05-22 11:47:27 -0400

    home, about and header

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>ABout</h2>
+    <p>
+      This app uses React, Redux, React Router, adn many other helpful
+      libraries.
+    </p>
+  </div>
+);
+export default AboutPage;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => (
+  <div className="jumbotron">
+    <h1>Education Administration</h1>
+    <p>React, Redux and React Router for ultra-responsive web apps.</p>
+    <Link to="about" className="btn btn-primary btn-lg">
+      Learn More
+    </Link>
+  </div>
+);
+export default HomePage;
